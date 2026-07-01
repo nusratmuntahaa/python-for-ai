8ca7fb874362e233219488d6e9cc677176b576b4
Nusrat Muntaha <nusratmuntahaa@gmail.com>
2026-07-01 23:11:33 +0600

    Updates

--- sales-analysis/helpers.py
+++ sales-analysis/helpers.py
@@ -7,3 +7,8 @@ def calculate_total(quantity, price):
 def format_currency(amount):
     """Format number as currency"""
     return f"${amount:,.2f}"
+
+# Run this file as a script to test the functions
+if __name__ == "__main__":
+    print(calculate_total(5, 10.99))
+    print(format_currency(1234.5678))
